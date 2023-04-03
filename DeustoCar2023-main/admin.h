6e188819ed3f0ab2524b66c74cef09cbe82c9ade
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2023-04-03 23:29:21 +0200

    Recuperacion de cabecera Admin

--- DeustoCar2023-main/admin.h
+++ DeustoCar2023-main/admin.h
@@ -1,10 +1,20 @@
+#include"coche.h"
+#include"usuario.h"
+
 #ifndef ADMIN_H_
 #define ADMIN_H_
 
 typedef struct{
-	int idAdmin;
-	char nombre[30];
 	char contrasenya[30];
 }Admin;
+
+Coche crearAutomovil();
+Coche modificarAutomovil();
+void visualizarAutomovil();
+void eliminarAutomovil();
+Usuario crearUsuario();
+Usuario modificarUsuario();
+void visualizarUsuario();
+void eliminarUsuario();
 
 #endif /* ADMIN_H_ */
