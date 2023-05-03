f23c3616c809c3b07245a0900c5b2b616a10d884
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2023-05-03 23:57:26 +0200

    Add files via upload

--- DeustoCar2023-main/usuario.c
+++ DeustoCar2023-main/usuario.c
@@ -3,49 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void anyadirUsuario(sqlite3 *db, int result, Usuario usuario) {
-	result = insertUsuario(db, usuario.nombre, usuario.email, usuario.contrasenya, usuario.saldo);
-	if (result != SQLITE_OK) {
-		printf("Error inserting new data\n");
-		printf("%s\n", sqlite3_errmsg(db));
-	}
-};
+void usmodscr(void){
+	
+} //Opciones para modificar aspectos del usuario [nombre,email,contrasenya,saldo, y permisos de admin. local]
 
-void modificarUsuario(sqlite3 *db, int result, Usuario usuario, char codigo[50]) {
-	result = deleteUsuario(db, codigo);
-	if (result != SQLITE_OK) {
-		printf("Error deleting Usuario\n");
-		printf("%s\n", sqlite3_errmsg(db));
-	}
+//Funciones
+Usuario getUser(sqlite3 *db, int result){
+	
+}
+void anyadirUsuario(sqlite3 *db, int result, Usuario usuario){
+	
+}
+void grantAdmin(sqlite3 *db, int result, FILE *f, Usuario usuario){
+	
+}
+void modificarUsuario(sqlite3 *db, int result, Usuario usuario){
+	
+}
+void eliminarUsuario(sqlite3 *db, int result){
+	
+}
 
-	result = insertUsuario(db, usuario.nombre, usuario.email, usuario.contrasenya, usuario.saldo);
-	if (result != SQLITE_OK) {
-		printf("Error inserting new data\n");
-		printf("%s\n", sqlite3_errmsg(db));
-	}
-};
-
-void eliminarUsuario(sqlite3 *db, int result, char codigo[50]) {
-	result = deleteUsuario(db, codigo);
-	if (result != SQLITE_OK) {
-		printf("Error deleting Usuario\n");
-		printf("%s\n", sqlite3_errmsg(db));
-	}
-};
-
-void eliminarUsuarios(sqlite3 *db, int result) {
-	result = deleteAllUsuarios(db);
-	if (result != SQLITE_OK) {
-		printf("Error deleting all users\n");
-		printf("%s\n", sqlite3_errmsg(db));
-	}
-};
-
-void imprimirUsuario(sqlite3 *db, int result, Usuario u)
-{
-	//La contrasenya no se imprime
-	printf("\nID DE USUARIO: %i \n", u.id);
-	printf("EMAIL: %s \n", u.email);
-	printf("NOMBRE: %s \n", u.nombre);
-	printf("SALDO: %i \n \n", u.saldo);
-};
+//Funciones Visuales
+Usuario creaUsuario(char nombre[30],char email[30],char contrasenya[30],int saldo){
+	
+}
+//Perfectas para malloc??:
+void visualizarUsuarios(sqlite3 *db, int result){
+	
+}
+void imprimirUsuarios(sqlite3 *db, int result){
+	
+}
--- DeustoCar2023-main/usuario.h
+++ DeustoCar2023-main/usuario.h
@@ -13,13 +13,18 @@ typedef struct{
 
 //Menus extras
 void usmodscr(void); //Opciones para modificar aspectos del usuario [nombre,email,contrasenya,saldo, y permisos de admin. local]
+
 //Funciones
+Usuario getUser(sqlite3 *db, int result);
 void anyadirUsuario(sqlite3 *db, int result, Usuario usuario);
 void grantAdmin(sqlite3 *db, int result, FILE *f, Usuario usuario);
 void modificarUsuario(sqlite3 *db, int result, Usuario usuario);
 void eliminarUsuario(sqlite3 *db, int result);
+
+//Funciones Visuales
+Usuario creaUsuario(char nombre[30],char email[30],char contrasenya[30],int saldo);
 //Perfectas para malloc??:
-void visualizarUsuarios(sqlite3 *db, int result);
-void imprimirUsuarios(sqlite3 *db, int result);
+void visualizarUsuarios(sqlite3 *db, int result); //Muestra en pantalla de 10 en 10, array de Usuuarios??
+void imprimirUsuarios(sqlite3 *db, int result); //Exporta todos los usuarios existentes en un .txt, array de Usuarios?
 
 #endif /* USUARIO_H_ */
