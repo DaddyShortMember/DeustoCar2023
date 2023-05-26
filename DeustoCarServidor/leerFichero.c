84823b2ef21e2e8a780c0fb1a42184b7e55e863e
ignacioechavarren <ignacioechavarren@opendeusto.es>
2023-05-27 01:30:25 +0200

    Ficheros

--- DeustoCarServidor/leerFichero.c
+++ DeustoCarServidor/leerFichero.c
@@ -6,5 +6,29 @@
  */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "leerFichero.h"
+#include "Admin.h"
 
+Admin leerFichero() {
+    FILE *f;
+    //lectura de ficheros caracter a caracter
+
+
+    f = fopen("fichero.txt", "r");
+
+   //falta codigo
+
+    fclose(f);
+
+
+
+    Admin a;
+    strcpy(a.nUsuario, nombreUsuario);
+    strcpy(a.password, contrasena);
+
+    return a;
+}
 
--- DeustoCarServidor/leerFichero.h
+++ DeustoCarServidor/leerFichero.h
@@ -4,10 +4,16 @@
  *  Created on: 27 may 2023
  *      Author: IGNAC
  */
+#include "Admin.h"
+
 
 #ifndef DEUSTOCARSERVIDOR_LEERFICHERO_H_
 #define DEUSTOCARSERVIDOR_LEERFICHERO_H_
 
+
+
+
+Admin readConfigFile();
 
 
 
